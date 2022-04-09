ead4c37c10210b755c5d745ae090d72ee72befbe
john fu <fysososo@outlook.com>
2022-04-09 17:43:02 +0800

    添加

--- Assets/Third Party/GPU DPP/GPUDPPTest.cs
+++ Assets/Third Party/GPU DPP/GPUDPPTest.cs
@@ -54,7 +54,7 @@ namespace GPUDPP
                     {
                         Debug.LogError(" Error result!");
                     }
-                }    
+                }
             }
         }
 
